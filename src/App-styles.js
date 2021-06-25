86f541f83c3a20afab29bfe1b8e497a58137e243
Danilsonunes <danilsonunes@gmail.com>
2021-06-25 14:05:41 -0300

    creado el botos para el cambio de tema de claro a oscuro, segundo input para operaciones y pequenas correcciones en el css

--- src/App-styles.js
+++ src/App-styles.js
@@ -1,4 +1,17 @@
-import styled from 'styled-components';
+import styled, {} from 'styled-components';
+
+export const lightTheme = {
+    body: "#fff",
+    fontColor: "#333",
+    bottonColor: "#efefef"
+}
+
+export const darkTheme = {
+    body: "#2f2a3a",
+    fontColor: "#fff",
+    bottonColor: "#2f2a3a"
+
+}
 
  export const Main = styled.div`
     top: 50%;
@@ -10,22 +23,33 @@ import styled from 'styled-components';
     text-align: center;
     border: 10px solid #101116;
     border-radius: 5px;
-    background: #2f2a3a;
+    background:${(props) => props.theme.body};
 `;
 
 export const Form = styled.form`
 
     input{
-        height: 75px;
-        width: 249px;
-        background: #2f2a3a;
+        height: 30px;
+        width: 214px;
+        background:${(props) => props.theme.body};
         border: 0px;
-        color: #fff;
+        color: ${(props) => props.theme.fontColor};
         text-align: right;
-        font-size: 20px;
+        font-size: 35px;
         font-weight: 500;
         letter-spacing: 1px;
+        margin-top: 75px;
+        margin-left: -10px;
+        
     }
+        input + input{
+            font-size: 10px;
+            color: #7d5dff;
+            height: 15px;
+            margin-bottom: 25px;
+            margin-top: 0px;
+            
+        }
    
 `
 export const Keypad = styled.div`
@@ -34,36 +58,37 @@ export const Keypad = styled.div`
     grid-auto-rows: minmax(60px, auto);
 `
 export const ClearButton = styled.button` 
+    font-size: 20px;
     grid-column: 1/3;
     grid-row: 1;
-    color: #7d67d7;
+    color: #7d5dff;
     margin: 5px;
     border: none;
     cursor: pointer;
     border-radius: 50%;
-    background: #2f2a3a;
+    background: ${(props) => props.theme.body};
 
 ` 
 
 export const EqualButton = styled.button `
     grid-column: 4/5;
     grid-row: 5;
-    color: #f1f1f1;
+    color: #7d5dff;
     margin: 5px;
     border: none;
     cursor: pointer;
     border-radius: 50%;
-    background: #7d67d7;
+    background: ${(props) => props.theme.bottonColor};
+    
 `
 
 export const BackspaceButton = styled.button `
-    color: #7d67d7;
+    color: ${(props) => props.theme.fontColor};
     margin: 5px;
     border: none;
     border-radius: 50%;
     cursor: pointer;
-    background: #2f2a3a;
-
+    background: ${(props) => props.theme.bottonColor};
 `
 
 export const Button = styled.button ` 
@@ -71,8 +96,43 @@ export const Button = styled.button `
     border: none;
     border-radius: 50%;
     cursor: pointer;
-    background: #2f2a3a;
-    color: #f1f1f1;
+    background: ${(props) => props.theme.bottonColor};
+    color: ${(props) => props.theme.fontColor};
     font-weight: 500;
 `
+export const ButtonFun = styled(Button)`
+    background: #efefef;
+    color: #000;
+`
+
+// -*-*-**-*-* acomodar css usando el componente de botton ya creado -*--*-*-*-*
+// export const Button = styled.button
+// `
+//     margin: 5px;
+//     border: none;
+//     cursor: pointer;
+//     border-radius: 50%;
+//     background: #2f2a3a;
+//     color: #f1f1f1;
+// `
+
+// export const ButtonCol = styled.button
+// `
+//     margin: 5px;
+//     font-size: 30px;
+//     border: none;
+//     cursor: pointer;
+//     border-radius: 50%;
+//     background: #2f2a3a;
+//     color: #f1f1f1;
+// `
+
+// export const ButtonBor = styled.button
+// `
+//     margin: 5px;
+//     border: none;
+//     cursor: pointer;
+//     border-radius: 50%;
+//     background: #2f2a3a;
+//     color: #7d5dff;
 
--- src/App.js
+++ src/App.js
@@ -5,27 +5,39 @@ import { Main,
          ClearButton,
          BackspaceButton,
          Button,
-         EqualButton 
+         EqualButton,
+         lightTheme,
+         darkTheme,
+         ButtonFun 
         
 }from "./App-styles"
+import { ThemeProvider } from "styled-components";
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   const [result, setResult] = useState("");
+  const [operation, setOperation] = useState("")
 
   const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
+    setOperation(operation.concat(e.target.name));
   }
   const clear = () => {
     setResult("")
+    setOperation("")
   } 
 
   const backspace = () => {
-    setResult(result.slice(0, -1))
+    setOperation(operation.slice(0, -1))
   } 
 
   const calculate = () => {
     try {
-      setResult(eval(result).toString())
+      setResult(eval(operation).toString())
     } catch (err) {
       setResult("Error")
     }
@@ -33,32 +45,36 @@ const App = () => {
   }
 
   return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
     <Main>
         <Form>
           <input type="text" value={result} />
+          <input type="text" value={operation}/>   
         </Form>
 
         <Keypad>
           <ClearButton onClick={clear} id="clear" >Clear</ClearButton>
           <BackspaceButton onClick={backspace} id="backspace" >C</BackspaceButton>
-          <Button name="/" onClick={handleClick} >&divide;</Button>
+          <ButtonFun name="/" onClick={handleClick} >&divide;</ButtonFun>
           <Button name="7" onClick={handleClick} >7</Button>
           <Button name="8" onClick={handleClick} >8</Button>
           <Button name="9" onClick={handleClick} >9</Button>
-          <Button name="*" onClick={handleClick} >&times;</Button>
+          <ButtonFun name="*" onClick={handleClick} >&times;</ButtonFun>
           <Button name="4" onClick={handleClick} >4</Button>
           <Button name="5" onClick={handleClick} >5</Button>
           <Button name="6" onClick={handleClick} >6</Button>
-          <Button name="-" onClick={handleClick} >&ndash;</Button>
+          <ButtonFun name="-" onClick={handleClick} >&ndash;</ButtonFun>
           <Button name="1" onClick={handleClick} >1</Button>
           <Button name="2" onClick={handleClick} >2</Button>
           <Button name="3" onClick={handleClick} >3</Button>
-          <Button name="+" onClick={handleClick} >+</Button>
+          <ButtonFun name="+" onClick={handleClick} >+</ButtonFun>
           <Button name="0" onClick={handleClick} >0</Button>
           <Button name="." onClick={handleClick} >.</Button>
+          <Button onClick={themeToggler} >color</Button>
           <EqualButton onClick={calculate} >=</EqualButton>
         </Keypad>
     </Main>
+    </ThemeProvider>
   );
 };
 
